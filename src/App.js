5c21644c084ba6a9eabe9c3cdc5d7cf4fe1c277a
Abhiyan <Abhiyanupreti76@gmail.com>
2022-08-28 15:45:26 +0545

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
-
-import Datavisualization from './components/Data Visualization/Datavisualization';
+import {useState} from 'react'
+// import Datavisualization from './components/Data Visualization/Datavisualization';
+import {Route,Routes,Link} from 'react-router-dom'
+import SignIn from './components/SignIn';
+import Home from './components/Home';
+import AccountPage from './components/AccountPage';
+import ProtectedRouts from './components/Protected';
 
 function App() {
   return (
     <>
-      <h1 className='title'> LineGraph</h1>
-      <Datavisualization/>
+    <Link to='/'>  Signin Page</Link>
+    <br />
+    <Link to='/home'>  Home Page</Link>
+    <br />
+    <Link to='/account'>  Account Page</Link>
+     <Routes>
+      <Route  path='/' element={<SignIn/>}/>
+      <Route element={<ProtectedRouts/>}>
+      <Route  path='/home' element={<Home/>}/>
+      <Route  path='/account' element={<AccountPage/>}/>
+      </Route>
+     </Routes>
     </>
   );
 }
---
+++ src/components/SignIn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <div>
+        <h1>Sign in Page</h1>
+    <h2> Can't navigate to other page until LoggedIn is true. </h2>
+    </div>
+  )
+}
